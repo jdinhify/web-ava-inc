2aa63b792146c92f52f3b6cce90d607891293b83
jdinhify <jdinhify@gmail.com>
2018-09-16 09:47:53 +1000

    ✨  used simple masonry instead of react gallery

--- modules/home/gallery.js
+++ modules/home/gallery.js
@@ -1,67 +1,88 @@
 import React from 'react'
-import Gallery from 'react-photo-gallery'
 
 const photos = [
   {
     src: '//files.jdinh.life/ava-inc-org-au/ava1.jpg',
-    width: 1836,
-    height: 1800,
   },
   {
     src: '//files.jdinh.life/ava-inc-org-au/ava2.jpg',
-    width: 960,
-    height: 720,
-  },
-  {
-    src: '//files.jdinh.life/ava-inc-org-au/ava3.jpg',
-    width: 960,
-    height: 720,
-  },
-  {
-    src: '//files.jdinh.life/ava-inc-org-au/ava4.jpg',
-    width: 960,
-    height: 714,
-  },
-  {
-    src: '//files.jdinh.life/ava-inc-org-au/ava5.jpg',
-    width: 960,
-    height: 720,
   },
   {
     src: '//files.jdinh.life/ava-inc-org-au/ava6.jpg',
-    width: 718,
-    height: 960,
   },
   {
-    src: '//files.jdinh.life/ava-inc-org-au/ava7.jpg',
-    width: 960,
-    height: 720,
-  },
-  {
-    src: '//files.jdinh.life/ava-inc-org-au/ava8.jpg',
-    width: 960,
-    height: 720,
-  },
-  {
-    src: '//files.jdinh.life/ava-inc-org-au/ava9.jpg',
-    width: 960,
-    height: 720,
-  },
-  {
-    src: '//files.jdinh.life/ava-inc-org-au/ava10.jpg',
-    width: 1024,
-    height: 683,
+    src: '//files.jdinh.life/ava-inc-org-au/ava4.jpg',
   },
   {
     src: '//files.jdinh.life/ava-inc-org-au/ava11.jpg',
-    width: 960,
-    height: 720,
+  },
+  {
+    src: '//files.jdinh.life/ava-inc-org-au/ava3.jpg',
+  },
+  {
+    src: '//files.jdinh.life/ava-inc-org-au/ava7.jpg',
+  },
+  {
+    src: '//files.jdinh.life/ava-inc-org-au/ava8.jpg',
+  },
+  {
+    src: '//files.jdinh.life/ava-inc-org-au/ava9.jpg',
+  },
+  {
+    src: '//files.jdinh.life/ava-inc-org-au/ava10.jpg',
+  },
+  {
+    src: '//files.jdinh.life/ava-inc-org-au/ava5.jpg',
   },
   {
     src: '//files.jdinh.life/ava-inc-org-au/ava12.jpg',
-    width: 1024,
-    height: 751,
   },
 ]
 
-export default () => <Gallery photos={photos} />
+export default () => (
+  <div className="masonry">
+    {photos.map(({ src }) => (
+      <figure className="masonry-brick" key={src}>
+        <img src={src} alt="" className="masonry-img" />
+      </figure>
+    ))}
+
+    {/* eslint-disable react/jsx-one-expression-per-line, react/jsx-closing-tag-location */}
+    <style jsx>{`
+      .masonry {
+        display: flex;
+        flex-flow: row wrap;
+        padding-left: .25rem;
+        padding-top: .25rem;
+        width: 100%;
+      }
+
+      .masonry-brick {
+        flex: auto;
+        min-width: 9rem;
+        padding: 0 .25rem .25rem 0;
+        margin: 0;
+      }
+
+      .masonry-brick:nth-child(4n+1){
+        width: 16rem;
+      }
+      .masonry-brick:nth-child(4n+2){
+        width: 20rem;
+      }
+      .masonry-brick:nth-child(4n+3){
+        width: 12rem;
+      }
+      .masonry-brick:nth-child(4n+4){
+        width: 24rem;
+      }
+
+      .masonry-img {
+        object-fit: cover;
+        height: 100%;
+        width: 100%;
+      }
+    `}</style>
+    {/* eslint-enable */}
+  </div>
+)
