43116efbd996ee82ca78d67cbe45db1fb8efce9e
Australian Vietnamese Arts <67725282+AVAMELB@users.noreply.github.com>
2020-07-08 23:36:07 +1000

    Fix spelling errors

--- components/home-what-we-do.js
+++ components/home-what-we-do.js
@@ -8,8 +8,8 @@ export const HomeWhatWeDo = () => (
         <h2>What we do</h2>
         <h3>Run festivals of Vietnamese Traditional Music </h3>
         <h3>Perform at nursing homes</h3>
-        <h3>Perform at Vietamese New Year festivals</h3>
-        <h3>Perform at Annual Mid Autumn Fesitvals</h3>
+        <h3>Perform at Vietnamese New Year festivals</h3>
+        <h3>Perform at Annual Mid Autumn Festivals</h3>
         <h3>Join Moomba Festivals</h3>
       </div>
     </div>
